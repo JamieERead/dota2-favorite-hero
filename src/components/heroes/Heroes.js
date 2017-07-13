e713fb4aacc6a0407c9546f623f01ac16ca23a75
jamie <jamieread@benefex.co.uk>
2017-07-13 17:21:18 +0100

    added toggle event for selecting favourite hero

--- src/components/heroes/Heroes.js
+++ src/components/heroes/Heroes.js
@@ -3,15 +3,17 @@ import axios from 'axios';
 
 function HeroItem(props) {
     const heroName = props.hero.name.replace('npc_dota_hero_', '');
+    const heroId = props.hero.id;
+    const iconClass = props.hero.favourite ? 'fa fa-star' : 'fa fa-star-o';
     return(
         <li className="hero">
-            <i className="star fa fa-star-o" aria-hidden="true" onClick={props.favoriteHero}></i>
+            <i className={iconClass} aria-hidden="true" onClick={() => props.toggleFavouriteHero(heroId)}></i>
             <img src={`http://cdn.dota2.com/apps/dota2/images/heroes/${heroName}_vert.jpg`} />
         </li>
     );
 }
 
-export default class HeroesList extends Component {
+class HeroesList extends Component {
     constructor(){
         super();
         this.state = {heroes: [], loading: true};
@@ -24,31 +26,36 @@ export default class HeroesList extends Component {
     loadHeroes(){
         axios.get('https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v1?key=3D7D20701E1BA0C6C66D11A76D95FA58')
             .then(({data}) => {
+                const heroes = data.result.heroes;
+                heroes.forEach(hero => hero['favourite'] = false);
                 this.setState({
-                    heroes: data.result.heroes,
+                    heroes: heroes,
                     loading: false
                 });
             })
             .catch(function(error) {
               console.log('Request failed', error)
             });
-
     }
 
-    favoriteHero = () => {
-        console.log('favoriteHero')
-    }
+    toggleFavouriteHero = (heroId) => {
+        // API call to save
+        const heroes = this.state.heroes.slice();
+        let currentHero = heroes.find(hero => hero.id === heroId);
+        currentHero.favourite = !currentHero.favourite;
+        this.setState({heroes: heroes});
+    };
 
     render() {
         const heroes = this.state.heroes;
         let listItems = [];
         if(!this.state.loading){
             listItems = heroes.map((hero) =>
-                <HeroItem key={hero.id.toString()} hero={hero} favoriteHero={this.favoriteHero}/>
+                <HeroItem key={hero.id.toString()} hero={hero} toggleFavouriteHero={this.toggleFavouriteHero}/>
             );
         }
         return <ul className="heros">{listItems}</ul>;
     }
 }
 
-// export default HeroesList;
+export default HeroesList;
