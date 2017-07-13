6204190a7218c6282c223cdba78de29aec926adc
jamie <jamieread@benefex.co.uk>
2017-07-13 13:28:52 +0100

    added favoruite hero css

--- src/components/heroes/Heroes.js
+++ src/components/heroes/Heroes.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 function HeroItem(props) {
-    return <li><img src={'http://cdn.dota2.com/apps/dota2/images/heroes/' + props.hero.name + '_sb.png'} /></li>;
+    const heroName = props.hero.name.replace('npc_dota_hero_', '');
+    return(
+        <li className="hero">
+            <i className="star fa fa-star-o" aria-hidden="true" onClick={props.favoriteHero}></i>
+            <img src={`http://cdn.dota2.com/apps/dota2/images/heroes/${heroName}_vert.jpg`} />
+        </li>
+    );
 }
 
 export default class HeroesList extends Component {
@@ -16,7 +22,7 @@ export default class HeroesList extends Component {
     }
 
     loadHeroes(){
-        axios.get('https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v1?key=3D7D20701E1BA0C6C66D11A76D95FA58') //{mode: 'no-cors'}
+        axios.get('https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v1?key=3D7D20701E1BA0C6C66D11A76D95FA58')
             .then(({data}) => {
                 this.setState({
                     heroes: data.result.heroes,
@@ -29,15 +35,19 @@ export default class HeroesList extends Component {
 
     }
 
+    favoriteHero = () => {
+        console.log('favoriteHero')
+    }
+
     render() {
         const heroes = this.state.heroes;
         let listItems = [];
         if(!this.state.loading){
             listItems = heroes.map((hero) =>
-                <HeroItem key={hero.id.toString()} hero={hero}/>
+                <HeroItem key={hero.id.toString()} hero={hero} favoriteHero={this.favoriteHero}/>
             );
         }
-        return <ul>{listItems}</ul>;
+        return <ul className="heros">{listItems}</ul>;
     }
 }
 
